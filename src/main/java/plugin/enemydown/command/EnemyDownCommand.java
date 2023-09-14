2da14c070ddae0a538887589d60db6fe7091a329
Umi java <hiroshiori.327@gmail.com>
2023-09-14 15:58:15 +0900

    ゲーム開始時に装備をネザライト一式になる。

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -3,12 +3,15 @@ package plugin.enemydown.command;
 import java.util.List;
 import java.util.SplittableRandom;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import plugin.enemydown.Main;
 
 public class EnemyDownCommand implements CommandExecutor {
@@ -21,13 +24,30 @@ public class EnemyDownCommand implements CommandExecutor {
    if(sender instanceof Player player){
      World world = player.getWorld();
 
-     player.setHealth(20);
-     player.setFoodLevel(20);
+     // プレイヤーの状態を初期化する。
+     initPlayerStatus(player);
 
      world.spawnEntity(getEnemySpawnLocation(player, world), getEnemy());
    }
 
     return false;
+  }
+
+  /**
+   * ゲームを始める前にプレイヤーの状態を設定する。
+   * 体力と空腹度を最大にして、装備はネザライト一式になる。
+   * @param player　コマンドを実行したプレイヤー
+   */
+  private void initPlayerStatus(Player player) {
+    player.setHealth(20);
+    player.setFoodLevel(20);
+
+    PlayerInventory inventory = player.getInventory();
+    inventory.setHelmet(new ItemStack(Material.NETHERITE_HELMET));
+    inventory.setChestplate(new ItemStack(Material.NETHERITE_CHESTPLATE));
+    inventory.setBoots(new ItemStack(Material.NETHERITE_BOOTS));
+    inventory.setLeggings(new ItemStack(Material.NETHERITE_LEGGINGS));
+    inventory.setItemInMainHand(new ItemStack(Material.NETHERITE_SWORD));
   }
 
   /**
@@ -37,7 +57,8 @@ public class EnemyDownCommand implements CommandExecutor {
    *
    * @param player　コマンドを実行したプレイヤー
    * @param world　コマンドを実行したプレイヤーが所属するワールド
-   * @return　敵の出現場所
+   * @return
+   * 敵の出現場所
    */
   private Location getEnemySpawnLocation(Player player, World world) {
     Location playerLocation = player.getLocation();
@@ -51,7 +72,8 @@ public class EnemyDownCommand implements CommandExecutor {
 
   /**
    * ランダムで敵を抽選して、その結果の敵を取得します。
-   * @return　敵
+   * @return
+   * 敵
    */
   private EntityType getEnemy() {
     List<EntityType> enemyList = List.of(EntityType.ZOMBIE,EntityType.SKELETON,EntityType.SPIDER);
