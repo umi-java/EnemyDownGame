dab65f1491a5e63ee83c49edcd476a322576b9e0
Umi java <hiroshiori.327@gmail.com>
2023-10-22 21:53:39 +0900

    myBatisを使ってinsertを実行し、ゲーム終了時にPlayerScoreが登録される状態にした。

--- src/main/java/plugin/enemydown/command/EnemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -75,15 +75,12 @@ public class EnemyDownCommand extends BaseCommand implements Listener {
         PlayerScoreMapper mapper = session.getMapper(PlayerScoreMapper.class);
         List<PlayerScore> playerScoreList = mapper.selectList();
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         for(PlayerScore playerScore : playerScoreList) {
-          LocalDateTime date = LocalDateTime.parse(playerScore.getRegisteredAt(), formatter);
-
           player.sendMessage(playerScore.getId() + " | "
               + playerScore.getPlayerName() + " | "
               + playerScore.getScore() + " | "
               + playerScore.getDifficulty() + " | "
-              + date.format(formatter));
+              + playerScore.getRegisteredAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         }
 
       }
@@ -220,24 +217,21 @@ public class EnemyDownCommand extends BaseCommand implements Listener {
             nowExecutingPlayer.getPlayerName() + " 合計 " + nowExecutingPlayer.getScore() + "点！",
             10,40,10);
 
-        try (Connection con = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/spigot_server",
-            "root",
-            "mal03per");
-            Statement statement = con.createStatement()) {
-            statement.executeUpdate(
-                "insert player_score(player_name, score, difficulty, registered_at)"
-                + "values('" + nowExecutingPlayer.getPlayerName() + "'," + nowExecutingPlayer.getScore() + ", '"
-                    + difficulty + "', now());" );
-
-        } catch (SQLException e) {
-          e.printStackTrace();
-        }
-
         spawnEntityList.forEach(Entity::remove);
         spawnEntityList.clear();
 
         removePotionEffect(player);
+
+        // スコア登録処理
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+          PlayerScoreMapper mapper = session.getMapper(PlayerScoreMapper.class);
+          mapper.insert(new PlayerScore(
+              nowExecutingPlayer.getPlayerName(),
+              nowExecutingPlayer.getScore(),
+              difficulty));
+
+        }
+
 
         return;
       }
